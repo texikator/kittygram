61f716aa42288496299ab9bbc353940dc9d87a73
texikator <wiktot@mail.ru>
2024-03-31 23:24:02 +0300

    fst

--- cats/views.py
+++ cats/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -17,3 +18,22 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def cat_detail(request, pk):
+
+    cat = get_object_or_404(Cat, pk=pk)
+   
+    if request.method in ['PUT', 'PATCH']:
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    serializer = CatSerializer(cat)
+    return Response(serializer.data, status=status.HTTP_200_OK)
